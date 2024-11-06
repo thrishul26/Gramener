3d8dc2eaeeb222db10cf31a0fba4069b7c6ee59b
Thrishul Reddy <109869677+thrishul26@users.noreply.github.com>
2024-11-06 14:51:09 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -1,20 +1,3 @@
-// json2html.js
-// export default function json2html(data) {
-//     let html = `<table data-user="thrishulkothapally26@gmail.com">
-//                     <thead>
-//                         <tr><th>Name</th><th>Age</th><th>Gender</th></tr>
-//                     </thead>
-//                     <tbody>`;
-
-//     data.forEach(item => {
-//         html += <tr><td>${item.Name || ''}</td><td>${item.Age || ''}</td><td>${item.Gender || ''}</td></tr>;
-//     });
-
-//     html += </tbody></table>;
-//    return html;
-// }
-
-// json2html.js
 export default function json2html(data) {
     let html = `<table data-user="thrishulkothapally26@gmail.com">
                     <thead>
