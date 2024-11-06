ef334232f6b8710b90365ff3b1adb9081ee0dffc
thrishul26 <thrishulkothapally26@gmail.com>
2024-11-06 14:28:59 +0530

    ADDED JSON

---
+++ json2html.js
@@ -0,0 +1,15 @@
+// json2html.js
+export default function json2html(data) {
+    let html = `<table data-user="thrishulkothapally26@gmail.com">
+                    <thead>
+                        <tr><th>Name</th><th>Age</th><th>Gender</th></tr>
+                    </thead>
+                    <tbody>`;
+
+    data.forEach(item => {
+        html += <tr><td>${item.Name || ''}</td><td>${item.Age || ''}</td><td>${item.Gender || ''}</td></tr>;
+    });
+
+    html += </tbody></table>;
+   return html;
+}
